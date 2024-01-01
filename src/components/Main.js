28751c092aadc0b97967a05caefd7782e7d44154
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2024-01-01 08:33:49 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -128,7 +128,7 @@ transition: height 0.5s ease, width 1s ease 0.5s;
 
 const Main = () => {
 
-    const [click, setClick] = useState(true);
+    const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
 
