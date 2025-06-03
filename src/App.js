d0db1f3edf298f591751df5fb7d893947e731035
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2025-06-03 22:32:48 +0300

    work menu update

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
 
             <Route path="/about" element={<AboutPage />} />
 
-            <Route path="/work" element={<WorkPage />} />
+            <Route path="/blog" element={<BlogPage />} />
 
             <Route path="/work" element={<WorkPage />} />
 
