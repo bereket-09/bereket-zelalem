6b323093e95dad0cbc9d1bc82a45cd70269f5431
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2025-06-03 22:30:08 +0300

    updates

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,6 +3,24 @@
 export const Work = [
     {
         id:1,
+        name:"IBEX USSD Platform",
+        description:"A USSD Platform Desinged for Safaricom Ethiopia Customers.",
+        tags:["NodeJs","Kubernties","MONGODB","Dragonfly","USSD"],
+        
+        demo:"https://arc.dev/@bereketyezelalem",
+        github:"https://arc.dev/@bereketyezelalem"
+    },
+      {
+        id:2,
+        name:"Kemer Learnign Managment System",
+        description:"E-Learnging Platform with full course sell and Chapa Integerations.",
+        tags:["Wordpress","LMS"],
+        
+        demo:"http://kemer-lms.rf.gd",
+        github:"http://kemer-lms.rf.gd"
+    },
+    {
+        id:3,
         name:"Kalab's Coffee",
         description:"A nice Website with Admin Dashboard and landing page for a coffe shop",
         tags:["Laravel","Filament","MYSQL"],
@@ -11,7 +29,7 @@ export const Work = [
         github:"https://github.com/bereket-09/Kalab-coffee"
     },
     {
-        id:2,
+        id:4,
         name:"ERP-Based-Clinic-Management-System-Laravel",
         description:"An ERP Based Clinic Managment System.",
         tags:["Laravel","MYSQL"],
@@ -21,7 +39,7 @@ export const Work = [
         
     },
     {
-        id:3,
+        id:5,
         name:"Multi Vendor Ecommerce",
         description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
         tags:["MERN Stack","Socket.io","SMPP"],
@@ -30,16 +48,16 @@ export const Work = [
         github:"https://github.com/bereket-09/E-Shop-MERN"
     },
     {
-        id:4,
+        id:6,
         name:"Habesha Bulk Markting",
         description:"A Multi Functional Bulk Marketing Tool",
         tags:["Laravel","Node JS","BULK SMS and SMPP"],
         
-        demo:"https://react-sidebar.vercel.app/",
+        demo:"https://youtu.be/DeidMUW8clw",
         github:"https://github.com/bereket-09/Habesha-Bulk-SMS-Manager"
        
     },{
-        id:5,
+        id:7,
         name:"SMS Based Survey Portal",
         description:"An Interactive SMS Based Survey Portal with Front end and Backend",
         tags:["Angular","Node JS","SMPP"],
@@ -49,7 +67,7 @@ export const Work = [
         
     },
     {
-        id:6,
+        id:8,
         name:"Cinima Ticket Booking System ",
         description:"A centalized protal to allow customer book thier favorite Cinima tickets",
         tags:["HTML","Js","css","PHP"],
